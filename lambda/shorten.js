bcf7d43bd787a8f75eff9d4c509815ac638f9274
Matt Beiswenger <mattbeis@yahoo.com>
2020-12-10 20:56:37 -0600

    feat: create initial example application

---
+++ lambda/shorten.js
@@ -0,0 +1,41 @@
+const fs = require("fs");
+const { clone } = require("isomorphic-git");
+const http = require("isomorphic-git/http/node");
+const path = require("path");
+const { shorten } = require("netlify-shortener");
+
+exports.handler = async function (event, context) {
+  if (event.httpMethod !== "POST") {
+    return { statusCode: 405, body: "Method Not Allowed" };
+  }
+
+  const { url, endpoint } = JSON.parse(event.body);
+  if (url === undefined) {
+    return {
+      statusCode: 404,
+      body: 'You must provide a "url" parameter',
+    };
+  }
+
+  const dir = path.join(process.cwd(), "shortener-clone");
+  await clone({
+    fs,
+    http,
+    dir,
+    corsProxy: "https://cors.isomorphic-git.org",
+    url: "https://github.com/mattbeiswenger/shortener",
+    ref: "main",
+    onAuth: () => {
+      return {
+        username: `${process.env.GITHUB_TOKEN}`,
+      };
+    },
+  });
+
+  const link = shorten(url, endpoint);
+
+  return {
+    statusCode: 200,
+    body: link,
+  };
+};
